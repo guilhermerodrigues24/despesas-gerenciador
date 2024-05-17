b8542bc4dd0b206a0d076c9aaceb643fc05fd37b
guilhermerodrigues24 <guilhermesp2409@gmail.com>
2024-05-17 08:02:21 -0300

    fix: indicando a JPA que pessoa não é uma classe persistente

--- src/main/java/despesas/gerenciador/model/Pessoa.java
+++ src/main/java/despesas/gerenciador/model/Pessoa.java
@@ -3,7 +3,9 @@ package despesas.gerenciador.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public abstract class Pessoa {
 
     @Id
