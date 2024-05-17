9e79ab01effea3fc894177497d7e7f33ffba083e
guilhermerodrigues24 <guilhermesp2409@gmail.com>
2024-05-17 08:13:04 -0300

    mapeando as classes Pessoa e Usuario

--- src/main/java/despesas/gerenciador/model/Pessoa.java
+++ src/main/java/despesas/gerenciador/model/Pessoa.java
@@ -4,8 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.*;
 
 @MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public abstract class Pessoa {
 
     @Id
--- src/main/java/despesas/gerenciador/model/Usuario.java
+++ src/main/java/despesas/gerenciador/model/Usuario.java
@@ -14,7 +14,7 @@ import java.util.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper = true)
 public class Usuario extends Pessoa {
     public static final String TABLE_NAME = "usuario";
 
