33a314b220f689b5815f8c6677a599917eeb55f0
guilhermerodrigues24 <guilhermesp2409@gmail.com>
2024-05-20 21:19:21 -0300

    feat: despesas

--- src/main/java/despesas/gerenciador/controller/ReceitaController.java
+++ src/main/java/despesas/gerenciador/controller/ReceitaController.java
@@ -1,2 +1,50 @@
-package despesas.gerenciador.controller;public class ReceitaController {
+package despesas.gerenciador.controller;
+
+import despesas.gerenciador.model.Despesa;
+import despesas.gerenciador.model.Receita;
+import despesas.gerenciador.service.DespesaService;
+import despesas.gerenciador.service.ReceitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/receita")
+@Validated
+public class ReceitaController {
+
+    @Autowired
+    private ReceitaService receitaService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Receita> buscarReceitaPorId(@PathVariable Long id) {
+        Receita receita = receitaService.buscarReceitaPorId(id);
+        return ResponseEntity.ok().body(receita);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> criarReceita(@RequestBody Receita receita) {
+        receitaService.criarReceita(receita);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(receita.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizarReceita(@RequestBody Receita receita, @PathVariable Long id) {
+        receita.setId(id);
+        receitaService.atualizarReceita(receita);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarReceita(@PathVariable Long id) {
+        receitaService.deletarReceita(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/despesas/gerenciador/model/Receita.java
+++ src/main/java/despesas/gerenciador/model/Receita.java
@@ -1,2 +1,53 @@
-package despesas.gerenciador.model;public class Receita {
+package despesas.gerenciador.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "receita")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Receita extends Entidade {
+
+    @Column(name = "nome", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String nome;
+
+    @Column(name = "descricao", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String descricao;
+
+    @Column(name = "valor", nullable = false)
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false, message = "O valor deve ser maior que 0")
+    private BigDecimal valor;
+
+    @Column(name = "data", nullable = false)
+    @NotNull
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @JsonIgnoreProperties("receitas")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_receita_id", nullable = false)
+    @JsonIgnoreProperties("receitas")
+    private TipoReceita tipoReceita;
 }
